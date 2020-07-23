ccf419cb8e4a2d49862b4bcc157992b7c2cc1229
Florian Kempenich <Flori@nKempenich.com>
2020-07-23 20:33:42 +0100

    Win/OSX Systray icon with Pystray

--- experiments/buildExecutablesWithPython/sandbox.py
+++ experiments/buildExecutablesWithPython/sandbox.py
@@ -1,22 +1,26 @@
-# import rum
-
-# class AwesomeStatusBarApp(rumps.App):
-#     @rumps.clicked("Preferences")
-#     def prefs(self, _):
-#         rumps.alert("jk! no preferences available!")
-
-#     @rumps.clicked("Silly button")
-#     def onoff(self, sender):
-#         sender.state = not sender.state
-
-#     @rumps.clicked("Say hi")
-#     def sayhi(self, _):
-#         rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
-
-# if __name__ == "__main__":
-#     AwesomeStatusBarApp("Awesome App").run()
-
+from pystray import Icon, Menu, MenuItem
 import rumps
+import pystray
+from PIL import Image, ImageDraw
+
+
+def create_systray_icon():
+    # Generate an image and draw a pattern
+    width = 400
+    height = 400
+    color1 = (0, 0, 0)
+    color2 = (200, 200, 200)
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle(
+        (width // 2, 0, width, height // 2),
+        fill=color2)
+    dc.rectangle(
+        (0, height // 2, width // 2, height),
+        fill=color2)
+
+    return image
+
 
 class AwesomeStatusBarApp(rumps.App):
     @rumps.clicked("Preferences")
@@ -31,12 +35,66 @@ class AwesomeStatusBarApp(rumps.App):
     def sayhi(self, _):
         rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
 
+
+state = False
+
+
+def on_clicked(icon, item):
+    global state
+    state = not item.checked
+
+
+def on_quit(icon):
+    icon.stop()
+
+
+def item_checkable():
+    return MenuItem(
+        'Checkable',
+        on_clicked,
+        checked=lambda item: state
+    )
+
+
+def item_quit():
+    return MenuItem(
+        'Quit',
+        on_quit
+    )
+
+
+# Update the state in `on_clicked` and return the new state in
+# a `checked` callable
+icon = Icon(
+    'test',
+    create_systray_icon(),
+    menu=Menu(
+        item_checkable(),
+        item_quit()
+    )
+)
+
+
+def systray_with_pystray():
+    icon.run()
+
+
+def systray_with_rumps():
+    AwesomeStatusBarApp("Awesome App").run()
+
+
 if __name__ == "__main__":
     import platform
-    if platform.system() == 'Darwin':
-        AwesomeStatusBarApp("Awesome App").run()
-    elif platform.system() == 'Windows':
-        print('Windows')
+
+    def on_mac_os():
+        return platform.system() == 'Darwin'
+
+    def on_windows():
+        return platform.system() == 'Windows'
+
+    if on_mac_os() or on_windows():
+        systray_with_pystray()
+        # systray_with_rumps()
     else:
         raise f'Platform Not Supported - {platform.system()}'
 
