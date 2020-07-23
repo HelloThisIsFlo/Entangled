fc5b2054aa25e007a1bdc0cb1e43c30b8f73eefd
Florian Kempenich <Flori@nKempenich.com>
2020-07-23 14:06:56 +0100

    Build dmg w/ 'appdmg'

--- experiments/buildExecutablesWithPython/build.sh
+++ experiments/buildExecutablesWithPython/build.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+function create_app_tkinter_version() {
+    # Prompting for sudo pass
+    sudo echo ""
 
-rm -rf $DIR/build $DIR/dist
+    pipenv run pyinstaller -w $DIR/sandbox.py
 
-# Prompting for sudo pass
-sudo echo ""
+    # Fix TCL bug: https://jacob-brown.github.io/2019-09-10-pyinstaller#macos
+    # Not needed if not using tkinter
+    sudo python $DIR/TCLChanger/TCLChanger.py
+}
 
-pyinstaller -w $DIR/sandbox.py
+function create_app() {
+    pipenv run pyinstaller -w --onefile $DIR/sandbox.py
+}
 
-# Fix TCL bug: https://jacob-brown.github.io/2019-09-10-pyinstaller#macos
-# Not needed if not using tkinter
-sudo python $DIR/TCLChanger/TCLChanger.py
+function build_dmg() {
+    # NPM Tool - Install if needed: 'npm install -g appdmg'
+    # Repo: https://github.com/LinusU/node-appdmg
+    appdmg $DIR/appdmg.json $DIR/dist/sandbox.dmg
+}
+
+function cleanup() {
+    rm -rf $DIR/build $DIR/dist
+}
+
+function show_result() {
+    open $DIR/dist
+}
+
+cleanup
+create_app
+build_dmg
+show_result
