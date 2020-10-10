5cc3eabfd1d336d2bca9367abd2a2abf9f9384ed
Florian Kempenich <Flori@nKempenich.com>
2020-10-10 22:17:08 +0100

    Add notes for next step

--- e2eTests/cypress/integration/entangled.spec.js
+++ e2eTests/cypress/integration/entangled.spec.js
@@ -21,4 +21,34 @@ context("Entangled", () => {
       expect(message).to.not.be.null;
     });
   });
+
+  /*
+  Notes:
+  We want to implement feature 1
+    - When click play it sends a 'play' message with:
+      - movie_time: current movie time
+      - play_at: timestamp of now + DELAY (configured at 5min for cypress, tweak in real life)
+
+  Question: 
+    - How to mock current movie time
+    - How to check delay ✅
+
+  # How to check delay
+  For this one, set a version of the system that configures the delay to 5min when running for Cypress.
+  Then in cypress just check the 'play_at' timestamp is 'now + 5min (+/- 10sec)'
+  - Create a launch flag '-env E2E|prod'
+
+  # How to mock the current movie time
+  Create a 'cypress-mock-interface' that contains:
+    - a text field: 'cypress-mock-movie-time'
+
+
+
+  Unrelated Note:
+  Check if possible to launch flask app from python. Much cleaner than the
+  current version that relies on the flask cli.
+  It'd allow to have a main.py file that'd call the server.py module instead
+  of relying on the 'entangled.py' (where all the routes are set) as the main file
+
+  */
 });
