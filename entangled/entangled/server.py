18aee1c13bdd2c18c8950d03597b85f88af887f3
Florian Kempenich <Flori@nKempenich.com>
2020-10-11 13:23:59 +0100

    Use app factory instead of global variable

--- entangled/entangled/server.py
+++ entangled/entangled/server.py
@@ -6,23 +6,26 @@ from entangled.mqtt import MQTTClient
 from entangled.logger import logger
 
 
-app = Flask(
-    __name__,
-    template_folder='../templates'
-)
+def initialize_app():
+    app = Flask(
+        __name__,
+        template_folder='../templates'
+    )
 
-mqtt_client = MQTTClient()
-mqtt_client.connect()
-atexit.register(mqtt_client.destroy)
-logger.info('module init')
+    mqtt_client = MQTTClient()
+    mqtt_client.connect()
+    atexit.register(mqtt_client.destroy)
+    logger.info('module init')
 
+    @app.route('/')
+    def main_page():
+        return render_template('main_page.html')
 
-@app.route('/')
-def main_page():
-    return render_template('main_page.html', example_var="frank")
+    @app.route('/play', methods=['POST'])
+    def play():
+        print(MQTTClient)
+        print(MQTTClient())
+        mqtt_client.send_message('play')
+        return 'playing'
 
-
-@app.route('/play', methods=['POST'])
-def play():
-    mqtt_client.send_message('play')
-    return 'playing'
+    return app
