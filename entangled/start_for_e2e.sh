1863bf387c0f88b4613aff243e5d2c34c9190bec
Florian Kempenich <Flori@nKempenich.com>
2020-10-11 19:29:45 +0100

    Remove dev mode for flask, it causes problem with MQTT constantly reconnecting

--- entangled/start_for_e2e.sh
+++ entangled/start_for_e2e.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 export FLASK_APP="entangled.server:initialize_app('e2e_tests')"
-export FLASK_ENV=development
+# export FLASK_ENV=development
 pipenv run flask run -p 7777
